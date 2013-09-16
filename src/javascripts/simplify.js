96544bf2c8d31c0ea523efdc17006e94633e7ddf
Inderpal Singh <inderps@thoughtworks.com>
2013-09-16 20:29:39 +0530

    few more changes

--- src/javascripts/simplify.js
+++ src/javascripts/simplify.js
@@ -1,5 +1,49 @@
 (function () {
     "use strict";
 
-    console.log($(window).height());
+    $(".screen").hide();
+    $(".main .screen").show();
+
+    $(window).scroll(function() {
+        var exit = false;
+        var windowHeight = $(window).height();
+        var scrollTop = $(document).scrollTop();
+
+        $(".page").each(function(index, element){
+            var pageHeight = $(element).position().top;
+            if((pageHeight - windowHeight + 400) < scrollTop){
+                exit = true;
+                hideIphone();
+                return;
+            }
+        });
+
+        if(exit){
+            return;
+        }
+
+        showIphone();
+        if(scrollTop < 300 && $(".main .screen:visible").length <= 0){
+            $(".screen").hide();
+            $(".main .screen").show();
+            return;
+        }
+         $(".feature").each(function(index, element){
+                var featureHeight = $(element).position().top;
+                 if((featureHeight - windowHeight + 400) > scrollTop || $(element).find(".screen:visible").length>0){
+                    return;
+                 }
+                $(".screen").hide();
+                $(element).find(".screen").show();
+         });
+    });
+
+    function hideIphone(){
+        $("#iphone").addClass("reallyHide");
+        $(".screen").hide();
+    }
+
+    function showIphone(){
+        $("#iphone").removeClass("reallyHide")
+    }
 })();
